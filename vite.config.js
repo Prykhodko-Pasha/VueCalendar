d3b232823a5c1f80b95a56fa4d2757eb1f80bedf
Prykhodko-Pasha <prykhodko.p@gmail.com>
2025-07-07 10:20:50 +0300

    turn back deploy fixing changes

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/VueCalendar/' : '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
-  }
 })
